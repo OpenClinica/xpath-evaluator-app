386429bc4dbd9f0906042e90b9cfdea94295f735
Martijn van de Rijdt <martijn@enketo.org>
2022-12-13 14:21:03 -0500

    changed: updated commander library

--- xpath-evaluate.js
+++ xpath-evaluate.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-'use strict';
-
-let program = require( 'commander' );
+const { Command } = require( 'commander' );
+const program = new Command();
 const pkg = require( './package' );
 const evaluateXPath = require( './src/evaluate' );
 const utils = require( './src/utils' );
@@ -14,40 +13,33 @@ const _output = ( issues = [], error = false ) => {
 };
 
 program
-    .usage( '<expression>' )
+    .argument( '<expression>', 'XPath expression to evaluate' )
     .option( '-x --xml <file>', 'path to XML file to evaluate expression on which is required if the context is provided' )
     .option( '-c --context <path>', 'path to XML context node for the evaluation' )
     .version( pkg.version )
-    .parse( process.argv );
-
-program.parse( process.argv );
-
-const expr = program.args[ 0 ];
-const xmlFile = program.xml;
-const context = program.context;
-
-if ( !expr ) {
-    console.error( 'Nothing to do. Missing expression. Use --help flag to see manual.' );
-    process.exit( 1 );
-} else if ( context && !xmlFile ) {
-    console.error( 'If a context is provided, an XML file has to be provided as well. Use --help flag to see manual.' );
-    process.exit( 1 );
-}
-
-
-if ( expr ) {
-    utils.getFileContents( xmlFile )
-        .then( xmlFile => evaluateXPath( xmlFile, expr, context ) )
-        .catch( ( errors = [] ) => {
-            errors = Array.isArray( errors ) ? errors : [ errors ];
-            _output( errors, true );
+    .action((expression, options) => {
+        
+        if (!expression){
+            console.error( 'Nothing to do. Missing expression. Use --help flag to see manual.' );
             process.exit( 1 );
-        } )
-        .then( ( result = '' ) => {
-            console.log( result );
-            process.exit( 0 );
-        } );
+        } else if ( options.context && !options.xml ) {
+            console.error( 'If a context is provided, an XML file has to be provided as well. Use --help flag to see manual.' );
+            process.exit( 1 );
+        }
+        
+        console.time('total');
+        utils.getFileContents( options.xml )
+            .then( xmlFile => evaluateXPath( xmlFile, expression, options.context ) )
+            .catch( ( errors = [] ) => {
+                errors = Array.isArray( errors ) ? errors : [ errors ];
+                _output( errors, true );
+                process.exit( 1 );
+            } )
+            .then( ( result = '' ) => {
+                console.timeEnd('total');
+                console.log( result );
+                process.exit( 0 );
+            } );
+    }); 
 
-} else {
-
-}
+    program.parse();
