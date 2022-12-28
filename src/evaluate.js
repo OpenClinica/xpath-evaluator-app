86855f1fd2a2ac35f002bb8d087a5ac0dc38033a
Martijn van de Rijdt <martijn@enketo.org>
2022-12-28 10:43:52 -0500

    changed: updated libaries

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,4 +1,4 @@
-const {DOMParser} = require( 'xmldom' );
+const {DOMParser} = require( '@xmldom/xmldom' );
 const {Evaluator} = require( '../build/evaluator-bundle' );
 
 // TODO: optimize performance
